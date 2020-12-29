8fddf03e90cdeffe4d90282ff0bd22bc2c25c883
Soldy <4786022+Soldy@users.noreply.github.com>
2020-12-29 19:32:45 +0000

    new eslint rule
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- index.js
+++ index.js
@@ -6,18 +6,18 @@ const retardBase = function () {
      * @public
      * @return {string}
      */
-     this.div = function (attr, ins) {
-         return make('div', attr, ins);
-     };
+    this.div = function (attr, ins) {
+        return div(attr, ins);
+    };
     /*
      * @param {object} 
      * @param {object} 
      * @public
      * @return {string}
      */
-     this.nav = function (attr, ins) {
-         return make('nav', attr, ins);
-     };
+    this.nav = function (attr, ins) {
+        return nav(attr, ins);
+    };
     /*
      * @param {object}
      * @public
@@ -27,7 +27,7 @@ const retardBase = function () {
         return full(
             elements
         );
-     };
+    };
     /*
      * @param {string} 
      * @param {object} 
@@ -35,72 +35,72 @@ const retardBase = function () {
      * @public
      * @return {string}
      */
-     this.make = function (element, attributions, inside) {
-         return make(
-             element,
-             attributions, inside
-         );
-     };
+    this.make = function (element, attributions, inside) {
+        return make(
+            element,
+            attributions, inside
+        );
+    };
     /*
      * @param {string}
      * @param {object}
      * @public
      * @return {string}
      */
-     this.single = function (element, attributions) {
-         return single(element, attributions);
-     };
+    this.single = function (element, attributions) {
+        return single(element, attributions);
+    };
     /*
      * @param {object} 
      * @private
      * @return {string}
      */
-     let full= function (elements){
-         let t = '';
-         if (typeof elements.e === 'undefined')
-             elements.e = 'div';
-         if (typeof elements.a === 'undefined')
-             elements.a = {};
-         if (typeof elements.i === 'undefined')
-             elements.i = '';
-         if(typeof elements.i === 'string'){
-             t=elements.i; 
-         }else if (typeof elements.i === 'object'){
-             if(Array.isArray(elements.i)){
-                 for(let n = 0 ; elements.i.length > n ; n++)
-                     t+=full(
-                         elements.i[n]
-                     );
-             }else{
-                 t = full(
-                     elements.i
-                 );
-             };
-         };
-         return make(
-             elements.e, 
-             elements.a, 
-             t
-         );
-     };
-    /*
-     * @param {object} 
-     * @param {object} 
-     * @private
-     * @return {string}
-     */
-     let nav = function (attr, ins) {
-         return make('nav', attr, ins);
-     };
+    let full= function (elements){
+        let t = '';
+        if (typeof elements.e === 'undefined')
+            elements.e = 'div';
+        if (typeof elements.a === 'undefined')
+            elements.a = {};
+        if (typeof elements.i === 'undefined')
+            elements.i = '';
+        if(typeof elements.i === 'string'){
+            t=elements.i; 
+        }else if (typeof elements.i === 'object'){
+            if(Array.isArray(elements.i)){
+                for(let n = 0 ; elements.i.length > n ; n++)
+                    t+=full(
+                        elements.i[n]
+                    );
+            }else{
+                t = full(
+                    elements.i
+                );
+            }
+        }
+        return make(
+            elements.e, 
+            elements.a, 
+            t
+        );
+    };
     /*
      * @param {object} 
      * @param {object} 
      * @private
      * @return {string}
      */
-     let div = function (attr, ins) {
-         return make('div', attr, ins);
-     };
+    let nav = function (attr, ins) {
+        return make('nav', attr, ins);
+    };
+    /*
+     * @param {object} 
+     * @param {object} 
+     * @private
+     * @return {string}
+     */
+    let div = function (attr, ins) {
+        return make('div', attr, ins);
+    };
     /*
      * @param {string} 
      * @param {object} 
@@ -108,60 +108,60 @@ const retardBase = function () {
      * @private
      * @return {string}
      */
-     let make = function (element, attributions, inside) {
-         if (typeof element === 'undefined')
-             return false;
-         if (typeof attributions === 'undefined')
-             attributions = {};
-         if (typeof inside === 'undefined')
+    let make = function (element, attributions, inside) {
+        if (typeof element === 'undefined')
+            return false;
+        if (typeof attributions === 'undefined')
+            attributions = {};
+        if (typeof inside === 'undefined')
             inside = '';
-         let out = '<' + element + ' ';
-         for (let attr in attributions)
+        let out = '<' + element + ' ';
+        for (let attr in attributions)
             out += attributionFull(
                 attr, 
                 attributions[attr]
             );
-         return out + '>' + inside + '</' + element + '>';
-     };
+        return out + '>' + inside + '</' + element + '>';
+    };
     /*
      * @param {string} 
      * @param {object} 
      * @private
      * @return {string}
      */
-     let single = function (element, attributions) {
-         if (typeof attributions === 'undefined')
-             attributions = {};
-         let out = '<' + element + ' ';
-         for (let attr in attributions)
+    let single = function (element, attributions) {
+        if (typeof attributions === 'undefined')
+            attributions = {};
+        let out = '<' + element + ' ';
+        for (let attr in attributions)
             out += attributionFull(
                 attr, 
                 attributions[attr]
             );
-         return out + '/>';
-     };
+        return out + '/>';
+    };
     /*
      * @param {string} 
      * @param {array}||{string} 
      * @private
      * @return {string}
      */
-     let attributionFull = function (attr, val) {
-         return '' + attr + '="' + attributionValue(val) + '" ';
-     };
+    let attributionFull = function (attr, val) {
+        return '' + attr + '="' + attributionValue(val) + '" ';
+    };
     /*
      * @param {array}||{string} 
      * @private
      * @return {string}
      */
-     let attributionValue = function (val) {
-         let out = '';
-         if (Array.isArray(val))
-             for (let i = 0; val.length > i; i++)
-                 out += val[i] + ' ';
-         else
-             out = val;
-         return out;
+    let attributionValue = function (val) {
+        let out = '';
+        if (Array.isArray(val))
+            for (let i = 0; val.length > i; i++)
+                out += val[i] + ' ';
+        else
+            out = val;
+        return out;
     };
 };
 
